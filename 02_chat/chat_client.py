1d089df3698f215fa4036d64788fa59b6fefbd5d
KMaster90 <73232282+KMaster90@users.noreply.github.com>
2023-03-24 12:16:39 +0100

    12. Basic Two-Way Chat Part 2 - Enabling The Chat

--- 02_chat/chat_client.py
+++ 02_chat/chat_client.py
@@ -10,3 +10,21 @@ BYTESIZE = 1024
 #Create a client socket and connect to the server
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client_socket.connect((DEST_IP, DEST_PORT))
+
+#Send/Receive messaged
+while True:
+    #Receive information from the server and print it
+    message = client_socket.recv(BYTESIZE).decode(ENCODER)
+
+    #Quit if the connected server wats to quit, else keep sending messages
+    if message == "quit":
+        client_socket.send("quit".encode(ENCODER))
+        print("Server has quit the connection")
+        break
+    else:
+        print(f"Server: {message}")
+        message = input("Message: ")
+        client_socket.send(message.encode(ENCODER))
+
+#Close the client socket
+client_socket.close()
--- 02_chat/chat_server.py
+++ 02_chat/chat_server.py
@@ -16,3 +16,21 @@ server_socket.listen()
 print("Server is running... \n")
 client_socket, client_address = server_socket.accept()
 client_socket.send("You are connected to the server".encode(ENCODER))
+
+#Send/Receive messaged
+while True:
+    #Receive information from the client and print it
+    message = client_socket.recv(BYTESIZE).decode(ENCODER)
+
+    #Quit if the connected client wats to quit, else keep sending messages
+    if message == "quit":
+        client_socket.send("quit".encode(ENCODER))
+        print("\nEnding the chat...goodbye!")
+        break
+    else:
+        print(f"\n{message}")
+        message = input("Message: ")
+        client_socket.send(message.encode(ENCODER))
+
+#Close the client socket
+server_socket.close()
